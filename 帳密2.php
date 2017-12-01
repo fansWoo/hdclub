eb1ca43c573765d61024743572e3dbb2a3ba7e0c
shaun-fanswoo <shaun@fanswoo.com>
2017-12-01 11:44:19 +0800

    updata 文件

---
+++ 帳密2.php
@@ -0,0 +1,76 @@
+
+MD5 dicuz 編碼 12345678 admin
+cb9ac438a1a1030be2809b964ae7e04c
+
+
+shaun@fanswoo.com
+fanswoo
+ad93f56f8692cc310d60d7a94892cf8e
+
+
+
+hd-club
+
+
+admin
+84141010076351430a9ba0df33253d63
+west
+a22bb35e9cdf3a24f38d033779bd94a4
+a22bb35e9cdf3a24f38d033779bd94a4
+
+
+dbpw: bulcdh
+
+uc_server/data中打开找到文件config.inc.php 以及 config/config_global.php、config/config_ucenter.php （这两个如果没有下面代码可以不用
+
+修改）
+
+define('UC_FOUNDERPW', '047099adb883dc19616dae0ef2adc5b6'); 
+define('UC_FOUNDERSALT', '311254'); 
+
+
+
+修改完后，Ucenter创始人的密码就变为: 123456789　
+
+define('UC_FOUNDERPW', '628fc7cde66d0ac27bfdd5f1036c29b8');
+define('UC_FOUNDERSALT', 'R9L2Ib');
+
+
+
+HD 原始密碼
+define('UC_FOUNDERPW', '1ddaa7033e33a1f8eb44044490d7823a');
+define('UC_FOUNDERSALT', '370296');
+
+
+
+給 HD Club 測試用的主機帳密
+
+cPanel 帳號：fanswoo-xxxx.com
+網域：fanswoo-xxxx.com
+FTP Host name：ftp.fanswoo-xxxx.com
+使用者：fanswoox
+密碼：!234Qwera
+
+member
+1
+admin@admin.com
+admin
+84141010076351430a9ba0df33253d63
+0
+0
+0
+0
+1
+1
+0
+1176787500
+19
+0
+9999
+0
+0
+0
+1
+0
+0
+0
