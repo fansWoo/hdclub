3da522b051784fd81ae912d7ced9906b3f19cc0b
Sena <shuanshuan@fanswoo.com>
2017-12-06 18:27:50 +0800

    手機板更新

--- template/sxp_phone1/touch/language/language.SC_GBK.php
+++ template/sxp_phone1/touch/language/language.SC_GBK.php
@@ -1,14 +1,14 @@
-<?php
-/* ---------------------------------
- * http://www.shangxuepai.com
-----------------------------------*/
-
-if(!defined('IN_DISCUZ')) {exit('Access Denied');}
-
-$sxp_lang = array(
-    1 => '删除收藏',
-	2 => '返回',
-	3 => '收藏主题',	
-	4 => '收藏时间：',
-);
+<?php
+/* ---------------------------------
+ * http://www.shangxuepai.com
+----------------------------------*/
+
+if(!defined('IN_DISCUZ')) {exit('Access Denied');}
+
+$sxp_lang = array(
+    1 => '删除收藏',
+	2 => '返回',
+	3 => '收藏主题',	
+	4 => '收藏时间：',
+);
 ?>
--- template/sxp_phone1/touch/language/language.TC_UTF8.php
+++ template/sxp_phone1/touch/language/language.TC_UTF8.php
@@ -1,14 +1,14 @@
-<?php
-/* ---------------------------------
- * http://www.shangxuepai.com
-----------------------------------*/
-
-if(!defined('IN_DISCUZ')) {exit('Access Denied');}
-
-$sxp_lang = array(
-    1 => '刪除收藏',
-	2 => '返回',
-	3 => '收藏主題',	
-	4 => '收藏時間：',
-);
+<?php
+/* ---------------------------------
+ * http://www.shangxuepai.com
+----------------------------------*/
+
+if(!defined('IN_DISCUZ')) {exit('Access Denied');}
+
+$sxp_lang = array(
+    1 => '刪除收藏',
+	2 => '返回',
+	3 => '收藏主題',	
+	4 => '收藏時間：',
+);
 ?>
